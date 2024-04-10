ef732a0336186271241b6667ecfa8240c159ead7
Miguel Buttendorf <miguelrbu@gmail.com>
2024-04-10 14:02:35 -0300

    Add StringFunctions file

---
+++ Curso_mySQL/StringFunctions.sql
@@ -0,0 +1,13 @@
+#Utilizando funções STRING
+
+#SELECT TRIM('   Carros ')
+#SELECT TRIM(BOTH 'a' FROM 'aaaaaCarrosaaaaaaa')
+#SELECT TRIM(LEADING 'a' FROM 'aaaaaCarrosaaaaaaa')
+#SELECT TRIM(TRAILING 'a' FROM 'aaaaaCarrosaaaaaaa')
+#SELECT LOCATE('c', 'Carros')
+#SELECT lcase('Carros')
+#SELECT ucase('Carros')
+#SELECT length('Carros')
+#SELECT repeat('Carros', 4)
+#SELECT right('Carros', 4) -- 4 ultimas letras
+SELECT left('Carros', 2) -- 2 primeiras letras
